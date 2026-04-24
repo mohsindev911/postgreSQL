27e25e4db64f6ec3f692acb737928daa171f16ea
mohsindev911 <mohsindev911@gmail.com>
2026-04-25 03:14:50 +0500

    integrate with postgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true,
+
+    }),
+    UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
